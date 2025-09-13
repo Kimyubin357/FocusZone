b61a8a6ba7f5bdab25aff7d67bb76f18cb78eb19
yheelearncode <yonghi01@naver.com>
2025-09-13 22:49:41 +0900

    add gesturehandler,m bottomsheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,21 @@ import { AuthProvider } from "@/src/services/auth/authContext";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <StatusBar style = "auto" />
-      <Stack screenOptions={{ headerShown: false, animation: "none" }}>
-        <Stack.Screen name="(protected)"/>
-        <Stack.Screen name="(auth)" />
-      </Stack>
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+      <AuthProvider>
+        <StatusBar style="auto" />
+        <Stack screenOptions={{ headerShown: false, animation: "none" }}>
+          <Stack.Screen name="(protected)" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </AuthProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
